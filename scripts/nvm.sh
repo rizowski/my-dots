8c75ee5b57dbffc8d545ec0814544a911a21d103
Colten Rouska <crouska@gmail.com>
2018-05-15 17:33:21 -0600

    nvm now downloads latest tag

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 source $PWD/scripts/helpers.sh
 
+function getLatestTag (){
+  curl -s https://api.github.com/repos/creationix/nvm/releases/latest | grep "\"name\":" | cut -d '"' -f 4
+}
+
+echo "installing or updating nvm..."
+curl -o- https://raw.githubusercontent.com/creationix/nvm/$(getLatestTag)/install.sh | zsh > /dev/null 2>&1
+
 load_nvm
 
-if ! command_exists nvm ; then
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | zsh
-
-  load_nvm
-
-  nvm i 8.10
-  nvm i 6.10
-  nvm alias default 8
-fi
+nvm i 8.10.0
+nvm i 6.10.3
+nvm alias default 8
